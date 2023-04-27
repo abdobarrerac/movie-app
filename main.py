0d9e2c6eb485ffcc770fdde8706ffac5806ad8f9
Abdo Enrique Barrera Contreras <abdobarrera@Abdos-MacBook-Pro.local>
2023-04-27 12:29:47 -0500

    getting movies

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 app.title = "Mi aplicación con FastApi"
 app.version = "0.0.1"
 
+movies = [
+    {
+        'id': 1,
+        'title': 'Avatar',
+        'overview': "En un exuberante planeta llamado Pandora viven los Na'vi, seres que ...",
+        'year': '2009',
+        'rating': 7.8,
+        'category': 'Acción'
+    }
+]
+
 @app.get('/', tags=['home'])
 def message():
-    return "Hello world Abdo"
+    return HTMLResponse('<h1>Hello World</h1>')
+
+@app.get('/movies', tags=[movies])
+def get_movies():
+    return movies
