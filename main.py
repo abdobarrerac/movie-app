3f617c547f53ca1cff3043cda8c08837ead92263
Abdo Enrique Barrera Contreras <abdobarrera@Abdos-MacBook-Pro.local>
2023-04-29 10:51:51 -0500

    new methods

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
 from fastapi.responses import HTMLResponse
 
 app = FastAPI()
@@ -39,3 +39,37 @@ def get_movie(id: int):
             return item
 
     return []
+
+@app.get('/movies/', tags=[movies])
+def get_movies_by_category(category: str, year: int):
+    return [item for item in movies if item['category'] == category]
+
+@app.post('/movies', tags=[movies])
+def create_movie(id: int = Body(), title: str = Body(), overview: str = Body(), year: int = Body(), rating: float = Body(), category: str = Body()):
+    movies.append({
+        "id": id,
+        "titel": title,
+        "overview": overview,
+        "year": year,
+        "rating": rating,
+        "category": category
+    })
+    return movies
+
+@app.put('/movies/{id}', tags=[movies])
+def update_movie(id: int, title: str = Body(), overview: str = Body(), year: int = Body(), rating: float = Body(), category: str = Body()):
+    for item in movies:
+        if item["id"] == id:
+            item['title'] = title,
+            item['overview'] = overview,
+            item['year'] = year,
+            item['rating'] = rating,
+            item['category'] = category,
+            return movies
+
+@app.delete('/movies/{id}', tags=[movies])
+def delete_movie(id: int):
+    for item in movies:
+        if item["id"] == id:
+            movies.remove(item)
+            return movies
