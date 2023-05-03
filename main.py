574fc73f1a7965c0ae81de567a822628c1d6ccd0
abdobarrerac <abarreracon@gmail.com>
2023-05-03 10:53:05 -0500

    implementing services and users router

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from fastapi.encoders import jsonable_encoder
 from middlewares.error_handler import ErrorHandler
 from middlewares.jwt_bearer import JWTBearer
 from routers.movie import movie_router
+from routers.users import user_router
 
 app = FastAPI()
 app.title = "Mi aplicación con FastApi"
@@ -17,42 +18,13 @@ app.version = "0.0.1"
 
 app.add_middleware(ErrorHandler)
 app.include_router(movie_router)
+app.include_router(user_router)
 
 #referencing database engine
 Base.metadata.create_all(bind=engine)
-
-class User(BaseModel):
-    email:str
-    password:str
-
-
-
-movies = [
-    {
-        'id': 1,
-        'title': 'Avatar',
-        'overview': "En un exuberante planeta llamado Pandora viven los Na'vi, seres que ...",
-        'year': '2009',
-        'rating': 7.8,
-        'category': 'Acción'
-    },
-    {
-        'id': 2,
-        'title': 'Avatar',
-        'overview': "En un exuberante planeta llamado Pandora viven los Na'vi, seres que ...",
-        'year': '2009',
-        'rating': 7.8,
-        'category': 'Acción'
-    }
-]
 
 @app.get('/', tags=['home'])
 def message():
     return HTMLResponse('<h1>Hello World</h1>')
 
-@app.post('/login', tags=['auth'])
-def login(user: User):
-    if user.email == "admin@gmail.com" and  user.password == "admin":
-        token: str = create_token(user.dict())
-        return JSONResponse(status_code=200, content=token)
 
